8da8055779b54ea1df31df2fbdb903c2438747d2
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-06-15 10:35:32 +0330

    [UPDATE] Accounting

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class UserManager(BaseUserManager):
     # Custom user model manager where email is the unique identifiers
     # for authentication instead of usernames.
@@ -52,21 +53,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    description = models.TextField()
-
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
